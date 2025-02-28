120c5a7d63af0dc317134904f3037f1deb1c04ff
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-02-28 09:21:46 +0800

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -23,6 +23,8 @@ app.use(
     })
 );
 
+app.options("*", cors());
+
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://my-chatext-client-two.vercel.app");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
