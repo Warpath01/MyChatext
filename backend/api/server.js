a6db297be8298083a84fea37e53a4438ae261a53
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-02-28 09:13:29 +0800

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -18,7 +18,7 @@ dotenv.config();
 // OR allow all origins (for development purposes)
 app.use(
     cors({
-        origin: "http://my-chatext.vercel-client-two.app",
+        origin: "https://my-chatext.vercel-client-two.app",
         credentials: true,
     })
 );
