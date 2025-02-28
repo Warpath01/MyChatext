1ee41801ee5a32ce3d120f62bc3d3d218461712d
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-02-28 09:00:29 +0800

    Update Users.jsx

--- frontend/src/components/Chat/Users.jsx
+++ frontend/src/components/Chat/Users.jsx
@@ -19,7 +19,7 @@ const Users = () => {
         className="d-flex flex-row justify-content-center w-100 border rounded shadow-sm align-items-center"
         style={{ gap: "10px" }}
       >
-        <FiUsers />
+      
         Online Contacts :{" "}
         <span>
           {users.filter((user) => onlineContacts.includes(user._id)).length}
