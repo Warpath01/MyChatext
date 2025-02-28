def22b034e134a42af02c3666ef270d4075c95cc
Warpath01 <114893752+Warpath01@users.noreply.github.com>
2025-02-28 09:20:31 +0800

    Update server.js

--- backend/api/server.js
+++ backend/api/server.js
@@ -23,6 +23,17 @@ app.use(
     })
 );
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://my-chatext-client-two.vercel.app");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
+
+
+
 app.use(express.json());
 app.use(cookieParser());
 
